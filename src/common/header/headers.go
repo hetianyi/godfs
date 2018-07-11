54dfcd4e0fb6752d4be400acc16f5beebe084f35
hetianyi <hehety@outlook.com>
2018-07-11 18:19:16 +0800

    basic files

--- src/common/header/headers.go
+++ src/common/header/headers.go
@@ -3,9 +3,7 @@ package header
 // Definition some data headers for data transfer.
 // [operation][meta][body...]
 type UploadHeader struct {
-    operation [8]byte   //operations such as upload, sync, communication
-    metaLen [10]
-    meta []
-    bodyLen []
-    body []
+    operation [8]byte   // operations such as upload, sync, communication.
+    metaLen [10]byte    // meta length for read meta.
+    bodyLen [10]byte    // body length for read body.
 }
