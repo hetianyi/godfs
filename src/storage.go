2a1ac013821d6420b3366ad20fd7db4dc188b0e1
Tianyi He <hehety@outlook.com>
2018-07-14 17:42:33 +0800

    roll log

--- src/storage.go
+++ src/storage.go
@@ -6,11 +6,19 @@ import (
     "util/logger"
     "lib_storage"
     "validate"
+    "flag"
+    "fmt"
 )
 
 func main() {
     s, _ := file.GetWorkDir()
-    m, e := file.ReadPropFile(s + string(filepath.Separator) + "conf" + string(filepath.Separator) + "storage.conf.template")
+    s = file.FixPath(s)
+    var confPath = flag.String("c", s + string(filepath.Separator) + ".." + string(filepath.Separator) + "conf" + string(filepath.Separator) + "storage.conf.template", "custom config file")
+
+    flag.Parse()
+    fmt.Println("confPath ", *confPath)
+
+    m, e := file.ReadPropFile(*confPath)
     if e == nil {
         validate.Check(m, 1)
         for k, v := range m {
