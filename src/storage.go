26c1dcc198136ad60c2b6d308cfb364a306ee076
Tianyi He <hehety@outlook.com>
2018-07-17 21:41:48 +0800

    http download

--- src/storage.go
+++ src/storage.go
@@ -7,6 +7,8 @@ import (
     "lib_storage"
     "validate"
     "flag"
+    "fmt"
+    "os"
 )
 
 // TODO 文件保存分为group和Initial memeber ID(文件初始成员ID)
@@ -14,6 +16,7 @@ import (
 // 并且恰好访问到没有同步完成的机器时，客户端会将请求重定向到文件原始服务器
 // exp: /G001(组)/M01(原始服务器)/{MD5}
 func main() {
+    fmt.Println(os.Args)
     s, _ := file.GetWorkDir()
     s = file.FixPath(s)
     var confPath = flag.String("c", s + string(filepath.Separator) + ".." + string(filepath.Separator) + "conf" + string(filepath.Separator) + "storage.conf.template", "custom config file")
