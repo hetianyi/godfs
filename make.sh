80bf5c005ce07a68dc8a9f9c9f20017883383b36
hetianyi <hehety@outlook.com>
2018-08-27 14:22:27 +0800

    update build scripts

--- make.sh
+++ make.sh
@@ -1,12 +1,22 @@
 #!/bin/sh
+echo "set go env."
 echo "$(go env)"|awk -F= '{export $1"="$2}'
 GOPATH="$PWD:/go:/go/src";
 export GOPATH
 
+echo "create build output directory."
 if [ ! -e "./bin" ];then
     mkdir ./bin
 fi
+
+echo "install go-sqlite3 lib..."
 go get github.com/mattn/go-sqlite3
+
+echo "build tracker..."
 go build -i -o $PWD/bin/tracker ./src/tracker.go
+
+echo "build storage..."
 go build -i -o $PWD/bin/storage ./src/storage.go
-#go build -i -o $PWD/bin/client ./src/client.go
+
+echo "build client..."
+go build -i -o $PWD/bin/client ./src/client.go
