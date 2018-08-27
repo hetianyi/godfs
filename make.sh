c9f1a26d332bcb1490ea09077234bf629edd9814
hetianyi <hehety@outlook.com>
2018-08-27 14:41:42 +0800

    prettify scripts

--- make.sh
+++ make.sh
@@ -1,22 +1,30 @@
 #!/bin/sh
-echo "set go env."
+
+echo -e "+-------------------------------------+"
+echo -e "|              >>godfs<<              |"
+echo -e "| focus project on github: \033[0m           |"
+echo -e "| \033[4mhttps://github.com/hetianyi/godfs \033[0m  |"
+echo -e "+-------------------------------------+"
+
+echo "step 1/6: set go env."
 echo "$(go env)"|awk -F= '{export $1"="$2}'
 GOPATH="$PWD:/go:/go/src";
 export GOPATH
 
-echo "create build output directory."
+echo "step 2/6: create build output directory."
 if [ ! -e "./bin" ];then
     mkdir ./bin
 fi
 
-echo "install go-sqlite3 lib..."
+echo "step 3/6: install go-sqlite3 lib..."
 go get github.com/mattn/go-sqlite3
 
-echo "build tracker..."
+echo "step 4/6: build tracker..."
 go build -i -o $PWD/bin/tracker ./src/tracker.go
 
-echo "build storage..."
+echo "step 5/6: build storage..."
 go build -i -o $PWD/bin/storage ./src/storage.go
 
-echo "build client..."
+echo "step 6/6: build client..."
 go build -i -o $PWD/bin/client ./src/client.go
+echo "build success!"
