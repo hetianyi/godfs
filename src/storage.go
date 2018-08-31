4524956773a533dddbf68dc5920e9dfb82074190
hetianyi <hehety@outlook.com>
2018-08-31 15:07:30 +0800

    add TODO item

--- src/storage.go
+++ src/storage.go
@@ -15,6 +15,7 @@ import (
 // 并且恰好访问到没有同步完成的机器时，客户端会将请求重定向到文件原始服务器
 // exp: /G001(组)/01(原始服务器实例ID)/M[S](单片or多片)/{MD5}[.ext]
 // 文件的原始名称需要客户端自行记录（可能未来加上服务端记录功能）
+// TODO support detect total file size for assigning
 func main() {
     abs, _ := filepath.Abs(os.Args[0])
     s, _ := filepath.Split(abs)
