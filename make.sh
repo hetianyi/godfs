8a542eaf9eda8d0da4c11e03d8392d5214626a6c
hetianyi <hehety@outlook.com>
2018-08-31 15:45:39 +0800

    update build script on linux

--- make.sh
+++ make.sh
@@ -8,25 +8,24 @@ echo -e "+-------------------------------------+"
 
 rm -rf bin
 
-echo "step 1/6: set go env."
 echo "$(go env)"|awk -F= '{export $1"="$2}'
 GOPATH="$PWD:/go:/go/src";
 export GOPATH
 
-echo "step 2/6: create build output directory."
+echo "step 1/5: create build output directory."
 if [ ! -e "./bin" ];then
     mkdir ./bin
 fi
 
-echo "step 3/6: install go-sqlite3 lib..."
+echo "step 2/5: install go-sqlite3 lib..."
 go get github.com/mattn/go-sqlite3
 
-echo "step 4/6: build tracker..."
+echo "step 3/5: build tracker..."
 go build -i -o $PWD/bin/tracker ./src/tracker.go
 
-echo "step 5/6: build storage..."
+echo "step 4/5: build storage..."
 go build -i -o $PWD/bin/storage ./src/storage.go
 
-echo "step 6/6: build client..."
+echo "step 5/5: build client..."
 go build -i -o $PWD/bin/client ./src/client.go
 echo "build success!"
