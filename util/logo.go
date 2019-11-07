6381609800d2805ba8dda1b32b54c508d76a03db
hehety <hehety@outlook.com>
2019-11-07 23:18:19 +0800

    update logo

--- util/logo.go
+++ util/logo.go
@@ -7,10 +7,10 @@ import (
 
 func PrintLogo() {
 	fmt.Print(`
-   ____  ____  _____  _____ ___
-  / ___\/ __ \/ ___ \/ ___/ ___| GoDFS::v` + common.VERSION + `
- / /_/\  /_/ / /__/ / /__/__  |  A distribute filesystem.
- \____/\____/____, /_/_______/   github.com/hetianyi/godfs
+   ____  ____  ____  _________
+  / ___\/ __ \/ __ \/ ___/ __/ GoDFS::v` + common.VERSION + `
+ / /_/\  /_/ / /_/ / /__/\ \   A distribute filesystem.
+ \____/\____/____./_/______/   github.com/hetianyi/godfs
 
 `)
 }
